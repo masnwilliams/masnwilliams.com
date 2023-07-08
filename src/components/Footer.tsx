d50e71d5656eb20b8592a1e461bbed59ee57ceb7
Mason Williams <mwilliams8979@gmail.com>
2023-07-08 19:19:29 -0400

    fixed footer and socials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
 const Footer = () => {
   return (
     <footer>
-      <div className={"flex justify-center mt-14 text-gray-500"}>
+      <div
+        className={
+          "flex justify-center mt-10 mb-5 text-sm sm:text-md text-gray-500 px-10 text-center"
+        }
+      >
         As you can probably tell, the website is a work in progress 🥲
       </div>
     </footer>
--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,32 +1,27 @@
 import Link from "next/link";
-import {
-  GitHubIcon,
-  LinkedInIcon,
-  ThreadsIcon,
-  TwitterIcon,
-} from "@/components/Icons";
+import { GitHubIcon, LinkedInIcon, TwitterIcon } from "@/components/Icons";
 
 const socialLinks = [
   {
     name: "GitHub",
     link: "https://www.github.com/masnwilliams",
-    icon: <GitHubIcon size={24} />,
+    icon: <GitHubIcon size={28} />,
   },
   {
     name: "LinkedIn",
     link: "https://www.linkedin.com/in/john-mason-williams/",
-    icon: <LinkedInIcon size={24} />,
+    icon: <LinkedInIcon size={28} />,
   },
   {
     name: "Twitter",
     link: "https://twitter.com/@masnwilliams",
-    icon: <TwitterIcon size={24} />,
+    icon: <TwitterIcon size={28} />,
   },
-  {
-    name: "Threads",
-    link: "https://threads.net/@johnmasonwilliams",
-    icon: <ThreadsIcon size={24} />,
-  },
+  // {
+  //   name: "Threads",
+  //   link: "https://threads.net/@johnmasonwilliams",
+  //   icon: <ThreadsIcon size={24} />,
+  // },
 ];
 
 const SocialLinks = () => {
