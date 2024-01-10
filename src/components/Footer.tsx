035b3efbd240a7cb96b897f5fb428ba7ca1aa98b
Mason Williams <mwilliams8979@gmail.com>
2024-01-09 22:15:44 -0500

    added confetti to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
           "flex justify-center mt-10 mb-5 text-sm sm:text-md text-gray-500 px-10 text-center"
         }
       >
-        Congrats! You have reached the end of the page.
+        Congrats! You have reached the end of the page 🎉
       </div>
     </footer>
   );
