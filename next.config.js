5fa1f429a1819fc802fbdb7ad40dcb2f20fd64cd
Mason Williams <mwilliams8979@gmail.com>
2024-08-23 14:30:18 -0400

    style: fix formatting and remove unnecessary code

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    redirects: async () => [
-        {
-            source: '/newsletter',
-            destination: 'https://learnbydoing.beehiiv.com/',
-            permanent: false,
-        },
-        // {
-        //     source: '/podcast',
-        //     destination: 'https://lbdpodcast.com/',
-        //     permanent: false,
-        // }
-    ],
+  redirects: async () => [
+    {
+      source: '/newsletter',
+      destination: 'https://learnbydoing.beehiiv.com/',
+      permanent: false,
+    },
+    {
+      source: '/podcast',
+      destination: 'https://podcasters.spotify.com/pod/show/coding-in-context',
+      permanent: false,
+    },
+  ],
 }
 
 module.exports = nextConfig
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,61 +1,62 @@
-"use client";
+'use client'
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import Image from "next/image";
-import SocialLinks from "@/components/SocialLinks";
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import Image from 'next/image'
+import SocialLinks from '@/components/SocialLinks'
 
 const navItems = [
   {
-    href: "/",
-    text: "Home",
-    newTab: false
+    href: '/',
+    text: 'Home',
+    newTab: false,
   },
   {
     href: '/newsletter',
     text: 'Newsletter',
-    newTab: true
+    newTab: true,
   },
-  // {
-  //   href: "/podcast",
-  //   text: "Podcast",
-  //   newTab: true
-  // }
-];
+  {
+    href: '/podcast',
+    text: 'Podcast',
+    newTab: true,
+  },
+]
 
 const NavBar = () => {
-  const pathname = usePathname();
+  const pathname = usePathname()
 
   const linkClasses = (href: string) => {
     return pathname === href
-      ? "text-blue-500 hover:text-blue-700"
-      : "text-black hover:text-gray-500";
-  };
+      ? 'text-blue-500 hover:text-blue-700'
+      : 'text-black hover:text-gray-500'
+  }
 
   return (
-    <div className={"flex px-32 justify-center sm:justify-between my-10 gap-5"}>
+    <div className={'flex px-32 justify-center sm:justify-between my-10 gap-5'}>
       <Link
-        href={"/"}
-        className={"hidden sm:inline hover:scale-105 transition-all"}
-      >
+        href={'/'}
+        className={'hidden sm:inline hover:scale-105 transition-all'}>
         <Image
-          src={"/mason.jpg"}
-          alt={"Me"}
+          src={'/mason.jpg'}
+          alt={'Me'}
           width={60}
           height={60}
-          className={"rounded-full"}
+          className={'rounded-full'}
         />
       </Link>
 
-      <div className={'flex font-bold items-center justify-evenly space-x-4 sm:space-x-8'}>
+      <div
+        className={
+          'flex font-bold items-center justify-evenly space-x-4 sm:space-x-8'
+        }>
         {navItems.map((item) => (
           <Link
             href={item.href}
             key={item.href}
             className={`${linkClasses(item.href)}`}
-            target={item.newTab ? "_blank" : ""}
-            rel={item.newTab ? "noopener noreferrer" : ""}
-          >
+            target={item.newTab ? '_blank' : ''}
+            rel={item.newTab ? 'noopener noreferrer' : ''}>
             {item.text}
           </Link>
         ))}
@@ -63,7 +64,7 @@ const NavBar = () => {
 
       <SocialLinks />
     </div>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
