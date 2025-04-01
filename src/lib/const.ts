cb474d70e847f06c06bc58e114e4dfa92c63f73a
Mason Williams <mwilliams8979@gmail.com>
2025-04-01 11:07:38 -0400

    feat: add Fathom project details and image file

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,76 +1,84 @@
-import { CardProps } from "@/lib/types";
+import { CardProps } from '@/lib/types'
 
 export const projects: CardProps[] = [
   {
-    title: "Learn By Doing",
-    image: "/lbd.png",
+    title: 'Fathom',
+    image: '/fathom.png',
+    description:
+      'AI-powered voice agents automating multilingual customer interactions, streamlining BPO operations, and making technology feel human.',
+    link: 'https://getfathom.ai/',
+    status: 'active',
+  },
+  {
+    title: 'Learn By Doing',
+    image: '/lbd.png',
     description:
       'Merging first-hand experiences from founders with business insights through a weekly newsletter and podcast.',
-    link: "/newsletter",
-    status: "active",
+    link: '/newsletter',
+    status: 'active',
   },
   {
-    title: "Forumify",
-    image: "/forumify.png",
+    title: 'Forumify',
+    image: '/forumify.png',
     description:
-      "Make your Discord forum channels available on the web, increasing SEO and brand visibility.",
-    link: "https://forumify.io/",
-    status: "active",
+      'Make your Discord forum channels available on the web, increasing SEO and brand visibility.',
+    link: 'https://forumify.io/',
+    status: 'active',
   },
   {
-    title: "BBR.gg",
-    image: "/bbr-gg.png",
+    title: 'BBR.gg',
+    image: '/bbr-gg.png',
     description:
-      "A companion app for the video game BattleBit Remastered, providing players with a platform to view stats, loadouts, and more.",
-    link: "https://bbr.gg/",
-    status: "unmaintained",
+      'A companion app for the video game BattleBit Remastered, providing players with a platform to view stats, loadouts, and more.',
+    link: 'https://bbr.gg/',
+    status: 'unmaintained',
   },
   {
-    title: "GreenScan",
+    title: 'GreenScan',
     image: undefined,
     description:
       "A product that provides validated information about a company's sustainability efforts, helping consumers make informed decisions.",
-    link: "https://greenscan.vercel.app/",
-    status: "unmaintained",
+    link: 'https://greenscan.vercel.app/',
+    status: 'unmaintained',
   },
   {
-    title: "APL.gg",
-    image: "/apl-gg.png",
+    title: 'APL.gg',
+    image: '/apl-gg.png',
     description:
-      "A centralized hub for the Apex Legends Esports community providing up-to-date news, tournament information, team standings, player profiles, and more.",
-    link: "https://apl.gg/",
-    status: "unmaintained",
+      'A centralized hub for the Apex Legends Esports community providing up-to-date news, tournament information, team standings, player profiles, and more.',
+    link: 'https://apl.gg/',
+    status: 'unmaintained',
   },
   {
-    title: "Bloxsmith",
+    title: 'Bloxsmith',
     image: undefined,
     description:
-      "The ultimate marketplace for game developers, offering seamless transactions of 3D Models, Graphics, UI, Maps, and more in both real and gaming currencies.",
-    link: "https://bloxsmith.com/",
-    status: "inactive",
+      'The ultimate marketplace for game developers, offering seamless transactions of 3D Models, Graphics, UI, Maps, and more in both real and gaming currencies.',
+    link: 'https://bloxsmith.com/',
+    status: 'inactive',
   },
   {
-    title: "StatShot",
+    title: 'StatShot',
     image: undefined,
     description:
-      "A simple web app for viewing player stats and matches for the video game Halo Infinite.",
-    link: "https://statshot.vercel.app/",
-    status: "unmaintained",
+      'A simple web app for viewing player stats and matches for the video game Halo Infinite.',
+    link: 'https://statshot.vercel.app/',
+    status: 'unmaintained',
   },
   {
-    title: "Space Foundry",
-    image: "/space-foundry.png",
+    title: 'Space Foundry',
+    image: '/space-foundry.png',
     description:
-      "A platform for players to buy and sell digital goods, aimed to be the platform of choice in an emerging market for all sales of digital goods.",
-    link: "https://space-foundry.com/",
-    status: "acquired",
+      'A platform for players to buy and sell digital goods, aimed to be the platform of choice in an emerging market for all sales of digital goods.',
+    link: 'https://space-foundry.com/',
+    status: 'acquired',
   },
   {
-    title: "Movie-ception",
-    image: "/movie-ception.jpg",
+    title: 'Movie-ception',
+    image: '/movie-ception.jpg',
     description:
-      "A web application that allows users to search, save, and view their favorite movies and shows, with recommendations based on user preferences.",
-    link: "https://movie-ception.vercel.app/",
-    status: "unmaintained",
+      'A web application that allows users to search, save, and view their favorite movies and shows, with recommendations based on user preferences.',
+    link: 'https://movie-ception.vercel.app/',
+    status: 'unmaintained',
   },
-];
+]
